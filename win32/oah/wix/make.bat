bd48bef0890293059cfa9f8b4fdb29146aa7c7b4
Haakon Sporsheim <haakon.sporsheim@tandberg.com>
2009-03-29 23:35:30 +0200

    [wix] Move wix to oah/wix and include bin directory in zlib.msm.

--- win32/oah/wix/make.bat
+++ win32/oah/wix/make.bat
@@ -1,6 +1,6 @@
 @echo off
-set RELEASE_PATH=%~dp0\..\oah\Win32\Release
-pkg-config --modversion %RELEASE_PATH%\lib\pkgconfig\zlib.pc > libver.tmp || goto error
+set RELEASE_PATH=%~dp0\..\Win32\Release
+%OAH_INSTALLED_PATH%bin\pkg-config --modversion %RELEASE_PATH%\lib\pkgconfig\zlib.pc > libver.tmp || goto error
 set /P LIBVER= < libver.tmp
 del libver.tmp
 
@@ -8,4 +8,5 @@ nmake /nologo version=%LIBVER% release_path=%RELEASE_PATH% %*
 
 goto:eof
 :error
+del libver.tmp
 echo Couldn't start build process... are pkg-config in your PATH and/or have you compiled z.sln with OAH_BUILD_OUTPUT cleared!??
